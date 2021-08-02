92ec26fc2cae2ce0bd835e3b5e973eea170a89bd
Cristian Escobar Castro <escobar.cristian@gmail.com>
2021-08-02 19:24:29 -0400

    tarea completada

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ function App() {
         <Home />    
         </p>      
       </header>
-      
-  
-
     </div>
   );
 }
--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,13 +1,22 @@
-import Formulario from "../../components/Formulario"
+import React from 'react'
 
-const Home = () => {
+const Home = () => {  
+    
+    const [boton, setValorBoton] = React.useState<string>("Mensaje1");
+    const [mensaje, setTexto] = React.useState<string>("Hola, bienvenido");
+
+    const handleModificaHeader = (e:any) => {       
+        e.preventDefault();
+        setValorBoton(boton=="Mensaje1" ? "Mensaje2" :"Mensaje1")
+        setTexto(boton=="Mensaje1" ? "Vuelve pronto!" : "Hola, bienvenido")
+    };
+
     return (
-        <div>          
-            <div >
-                <Formulario></Formulario>
-            </div>          
-        </div>
+        <header>              
+          <h1>{mensaje}</h1>         
+          <button id="boton" onClick={handleModificaHeader}>Salir</button>  
+      </header>
     )
-};
+}
 
-export default Home
+export default Home;
